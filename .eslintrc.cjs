602856373e860de5a759db353ea042fd4b97a87a
Heloi Neto <hey@heloineto.com>
2022-07-30 10:47:16 -0300

    chore: added Storybook, removed pnpm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,10 +7,16 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
 		"plugin:@typescript-eslint/strict",
 		"prettier",
+		"plugin:storybook/recommended",
 	],
 	plugins: ["svelte3", "@typescript-eslint"],
 	ignorePatterns: ["*.cjs"],
-	overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+	overrides: [
+		{
+			files: ["*.svelte"],
+			processor: "svelte3/svelte3",
+		},
+	],
 	settings: {
 		"svelte3/typescript": () => require("typescript"),
 	},
