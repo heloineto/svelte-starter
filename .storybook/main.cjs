49125fee65dcf12484c462bc3262c1c4ffa89ce5
Heloi Neto <hey@heloineto.com>
2022-07-30 10:55:08 -0300

    feat: added comment on storybook/main.csj

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,21 +1,19 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/svelte",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  // "svelteOptions": {
-  //   "preprocess": require("../svelte.config.js").preprocess
-  // },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+	],
+	framework: "@storybook/svelte",
+	core: {
+		builder: "@storybook/builder-vite",
+	},
+	// This doesn't work because `require``is not ESM
+	// "svelteOptions": {
+	//   "preprocess": require("../svelte.config.js").preprocess
+	// },
+	features: {
+		storyStoreV7: true,
+	},
+};
